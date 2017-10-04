82c10796c0090c3119fdae19ac506de6e27277d0
Robin Varghese <robin.codein@gmail.com>
2017-10-04 08:57:07 -0400

    settings.py
    can be overriden with local_settings.py
    same as how Django handles the same problem.

---
+++ settings.py
@@ -0,0 +1,19 @@
+import os
+
+GITHUB_OAUTH_TOKEN = 'GITHUB_OAUTH_TOKEN'
+
+
+
+##################
+# LOCAL SETTINGS #
+##################
+
+# DO NOT PLACE ANYTHING BELOW THIS!!!!!!!
+# Allow any settings to be defined in local_settings.py, which should be
+# ignored in your version control system, allowing for settings to be
+# defined per machine.
+try:
+    from local_settings import *  # NOQA
+except ImportError as e:
+    if "local_settings" not in str(e):
+        raise e
